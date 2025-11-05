a42470e3704ea1445557e1aed3d221f4c42b691a
Raul Hidalgo <raul.hidalgo@alumnos.ucn.cl>
2025-11-05 20:53:43 -0300

    feat: add interface token service and token service

--- src/Services/TokenService.cs
+++ src/Services/TokenService.cs
@@ -1,12 +1,111 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using authService.src.models;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using authService.src.interfaces;
 
 namespace authService.src.Services
 {
-    public class TokenService
+    public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _signingKey;
+
+        private static List<string> _Tokens = new List<string>();
+
+        public TokenService(){
+            var signingKey = Environment.GetEnvironmentVariable("JWT_SIGNING_KEY") ?? throw new ArgumentNullException("JWT_SIGNING_KEY environment variable is not set.");
+
+            _signingKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(signingKey));
+        }
+
+        public string GenerateToken(Login login)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, login.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            if(login.Roles != null)
+            {
+                foreach(var role in login.Roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+            }
+
+            var creds = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? throw new ArgumentNullException("JWT Issuer cannot be null or empty.");
+            var audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? throw new ArgumentNullException("JWT Audience cannot be null or empty.");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = creds,
+                Issuer = issuer,
+                Audience = audience
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            _Tokens.Add(tokenHandler.WriteToken(token));
+
+
+            return tokenHandler.WriteToken(token);
+
+
+
+
+        }
+
+
+        public void RevokeToken(string token)
+        {
+            _Tokens.Remove(token);
+        }
+
+        public ClaimsPrincipal? ValidateToken(string token)
+        {
+            if(!_Tokens.Contains(token))
+                return null;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? throw new ArgumentNullException("JWT Issuer cannot be null or empty.");
+            var audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? throw new ArgumentNullException("JWT Audience cannot be null or empty.");
+            
+
+            try
+            {
+                var validation = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = _signingKey,
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return validation;
+
+            }
+
+            catch
+            {
+                return null;
+            }
+        }
+
         
     }
 }
---
+++ src/interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using authService.src.models;
+using System.Security.Claims;
+
+namespace authService.src.interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateToken(Login login);
+        void RevokeToken(string token);
+        ClaimsPrincipal? ValidateToken(string token);
+    }
+}
